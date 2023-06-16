9cc26b6e0cfb0ac75e5fd5e2ee92973971d90847
Hamza <anouihamza@gmail.com>
2023-06-16 14:15:31 -0700

    Pirn4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
+
 /**
- * main - Empty point
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char c;
-
 	char d;
 
 	c = 'a';
 	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'z') {
-			putchar (d);
-			d++;
-		}
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'z')
+	{
+		putchar(d);
+		d++;
+	}
 	putchar('\n');
 	return (0);
 }
