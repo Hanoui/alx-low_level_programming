6d8fab3c55996a7b0a56a14180e0dff5d51254ab
Hamza <anouihamza@gmail.com>
2023-06-16 14:49:21 -0700

    Yeah

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Description: 'print alphabets lowercase and uppercase'
- * Return: always 0
+ * main - Prints the alphabet in lowercase
+ *        followed by a new line, except q and e
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-        int n = 97;
-        int m = 65;
+	char la;
 
-        while (n <= 122)
-        {
-                putchar(n);
-		n++;
-        }
-        while (m <= 90)
-        {
-                putchar(m);
-		m++;
-        }
-        putchar('\n');
-        return (0);
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'c' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
