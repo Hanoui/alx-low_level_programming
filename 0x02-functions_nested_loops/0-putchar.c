0a8032222945d700df216001aa32ea3369946f80
Hamza <anouihamza@gmail.com>
2023-06-20 13:33:37 -0700

    tup

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,11 @@
  */
 int main(void)
 {
-    char str[] = "_putchar\n";
-    int i;
+	char str[] = "_putchar\n";
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-        write(1, &str[i], 1);
+	for (i = 0; str[i] != '\0'; i++)
+		write(1, &str[i], 1);
 
-    return (0);
+	return (0);
 }
-
