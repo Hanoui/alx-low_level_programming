d71b6de6ea04bbda88340f040d47faf6c4bbdcf9
Hamza <anouihamza@gmail.com>
2023-06-20 14:03:11 -0700

    Hj

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,14 @@
 
 /**
  * print_alphabet - Prints the alphabet in lowercase followed by a new line
- *
- * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
-{
-	int i;
 
-	for (i = 97; i <= 122; i++)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(i);
 	}
