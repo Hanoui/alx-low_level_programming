999f39f8badbef20395ada19235dd045e8e8d2dc
Hamza <anouihamza@gmail.com>
2023-06-20 14:08:09 -0700

    Pui

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,14 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase followed by a new line
  */
-
 void print_alphabet(void)
-
 {
-	char c;
+    char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
 }
+
