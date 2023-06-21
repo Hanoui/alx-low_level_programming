0c86e6710920897c9e6f2ec41f45e875ad714ffb
Hamza <anouihamza@gmail.com>
2023-06-21 10:57:44 -0700

    Times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the n times table
- * @n: The number of times
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number of times table to print
  *
  * Return: void
  */
 void print_times_table(int n)
 {
-	int i, j, res;
+	int i, j, result;
 
 	if (n < 0 || n > 15)
 		return;
@@ -17,33 +17,35 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			res = i * j;
+			result = i * j;
 
-			if (j != 0)
+			if (j == 0)
+				_putchar('0');
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
 
-				if (res < 10)
+				if (result < 10)
 					_putchar(' ');
+				else if (result >= 10 && result < 100)
+					_putchar('0' + (result / 10));
 
-				if (res < 100)
-					_putchar(' ');
+				_putchar('0' + (result % 10));
 			}
-
-			if (res >= 100)
-			{
-				_putchar((res / 100) + '0');
-				_putchar(((res / 10) % 10) + '0');
-			}
-			else if (res >= 10)
-			{
-				_putchar((res / 10) + '0');
-			}
-
-			_putchar((res % 10) + '0');
 		}
 
 		_putchar('\n');
 	}
+}
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1. On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
