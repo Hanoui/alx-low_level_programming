7460ca1575ac2f72f1eed13950ee6af739133cbd
Hamza <anouihamza@gmail.com>
2023-06-21 11:02:50 -0700

    Times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,20 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * print_times_table - Prints n, starting with 0
- * @n: The number of times table to print
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_times_table - Prints n time, starting with 0
+ * @n: The number to print the times table for
  *
  * Return: void
  */
@@ -18,7 +30,6 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			result = i * j;
-
 			if (j == 0)
 				_putchar('0');
 			else
@@ -27,25 +38,44 @@ void print_times_table(int n)
 				_putchar(' ');
 
 				if (result < 10)
+				{
 					_putchar(' ');
-				else if (result >= 10 && result < 100)
-					_putchar('0' + (result / 10));
-
-				_putchar('0' + (result % 10));
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else if (result < 100)
+				{
+					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
 			}
 		}
-
 		_putchar('\n');
 	}
 }
 
 /**
- * _putchar - Writes a character to stdout
- * @c: The character to print
+ * main - Entry point
  *
- * Return: On success 1. On error, -1 is returned, and errno is appropriately
+ * Return: Always 0
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+
+	return (0);
 }
+
