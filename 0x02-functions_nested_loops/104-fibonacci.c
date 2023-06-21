288b5096f3a41771b6cc1b8a3bd752940a5c495d
Hamza <anouihamza@gmail.com>
2023-06-21 13:20:51 -0700

    Fub

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long int prev = 1, current = 2, next;
+
+	printf("%lu, %lu", prev, current);
+
+	for (count = 3; count <= 98; count++)
+	{
+		next = prev + current;
+		printf(", %lu", next);
+
+		prev = current;
+		current = next;
+	}
+
+	printf("\n");
+
+	return 0;
+}
