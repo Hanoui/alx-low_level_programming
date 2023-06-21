7ba9b5689b889ca437b0b600f998f64fa91cd6fd
Hamza <anouihamza@gmail.com>
2023-06-21 11:23:11 -0700

    Times

---
+++ 0x02-functions_nested_loops/main_0.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    print_times_table(2);
+    print_times_table(10);
+    print_times_table(13);
+    print_times_table(-2);
+    print_times_table(0);
+    print_times_table(100);
+
+    return (0);
+}
