37c1f5a3f05a13368d66e7ac982e57d26bccf12b
Hamza <anouihamza@gmail.com>
2023-06-21 15:50:52 -0700

    final

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(void)
 {
     int count;
-    unsigned long fib1 = 0, fib2 = 1, sum;
-    unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-    unsigned long half1, half2;
+    uint64_t fib1 = 0, fib2 = 1, sum;
+    uint64_t fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+    uint64_t half1, half2;
 
     for (count = 0; count < 92; count++)
     {
