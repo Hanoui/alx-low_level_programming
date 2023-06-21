49485fb385c9d87de04da4c03c8f0b4099d68802
Hamza <anouihamza@gmail.com>
2023-06-21 13:15:01 -0700

    Fib

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    unsigned long int prev = 1, current = 2, next;
+    unsigned long int sum = 0;
+
+    while (current <= 4000000)
+    {
+        if (current % 2 == 0)
+            sum += current;
+
+        next = prev + current;
+        prev = current;
+        current = next;
+    }
+
+    printf("%lu\n", sum);
+
+    return 0;
+}
+
