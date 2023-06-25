81fd94d40be3653b76b6c5b9f973aa031bebfad7
Hamza <anouihamza@gmail.com>
2023-06-25 15:43:28 -0700

    Final

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -19,9 +19,8 @@ void print_square(int size);
 int _putchar(char c);
 void print_triangle(int size);
 long int largest_prime_factor(long int n);
-void _putchar(char c);
-void print_number(int n);
 int _putchar(char c);
+void print_number(int n);
 
 
 #endif /* MAIN_H */
