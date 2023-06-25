bbe38495018229ec772ff01307e434fe3bd41c4a
Hamza <anouihamza@gmail.com>
2023-06-25 07:56:45 -0700

    Supper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,29 +2,5 @@
 #define MAIN_H
 
 int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-int putchar(int c);
-int _putchar(char c);
-void print_most_numbers(void);
-int _putchar(char c);
-void more_numbers(void);
-int _putchar(char c);
-void print_line(int n);
-int _putchar(char c);
-void print_diagonal(int n);
-int _putchar(char c);
-void print_square(int size);
-int _putchar(char c);
-void fizz_buzz(void);
-
-
-int _putchar(char c);
-void print_triangle(int size);
-void _putchar(char c);
-int _putchar(char c);
-void print_number(int n);
-int _putchar(char c);
 
 #endif /* MAIN_H */
