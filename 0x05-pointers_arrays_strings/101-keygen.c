31ba8778bcdb1d9a8c20adc86cc0b55c9b4d5be0
Hamza <anouihamza@gmail.com>
2023-06-26 11:09:50 -0700

    Keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,30 +9,30 @@
  */
 int main(void)
 {
-	int i, sum, rand_num;
-	char password[84];
+    int i, sum, rand_num;
+    char password[84];
 
-	sum = 0;
-	srand(time(0));
+    sum = 0;
+    srand(time(0));
 
-	for (i = 0; i < 83; i++)
-	{
-		rand_num = rand() % 62;
-		if (rand_num < 10)
-			password[i] = '0' + rand_num;
-		else if (rand_num < 36)
-			password[i] = 'A' + (rand_num - 10);
-		else
-			password[i] = 'a' + (rand_num - 36);
+    for (i = 0; i < 83; i++)
+    {
+        rand_num = rand() % 62;
+        if (rand_num < 10)
+            password[i] = '0' + rand_num;
+        else if (rand_num < 36)
+            password[i] = 'A' + (rand_num - 10);
+        else
+            password[i] = 'a' + (rand_num - 36);
 
-		sum += password[i];
-	}
+        sum += password[i];
+    }
 
-	password[i] = '0' + (sum % 10); // Fix: Calculate last character
+    password[i] = '0' + (sum % 10); /* Fix: Calculate last character */
 
-	password[i + 1] = '\0'; // Null-terminate the password
+    password[i + 1] = '\0'; // Null-terminate the password
 
-	printf("%s\n", password);
+    printf("%s\n", password);
 
-	return (0);
+    return (0);
 }
