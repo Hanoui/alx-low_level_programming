3bc47f790f42b51d95222989931012562ffe327b
Hamza <anouihamza@gmail.com>
2023-06-29 12:55:34 -0700

    Pkd

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,22 +6,20 @@
  */
 void print_number(int n)
 {
-    unsigned int num;
+	unsigned int num;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        num = -n;
-    }
-    else
-    {
-        num = n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
 
-    if (num / 10 != 0)
-    {
-        print_number(num / 10);
-    }
+	if (num / 10)
+		print_number(num / 10);
 
-    _putchar('0' + num % 10);
+	_putchar((num % 10) + '0');
 }
