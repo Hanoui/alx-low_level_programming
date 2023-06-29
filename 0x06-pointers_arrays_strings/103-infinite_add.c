966e57ac0c8b9972b943978423907939fc3ede95
Hamza <anouihamza@gmail.com>
2023-06-29 13:35:40 -0700

    klee

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,61 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * infinite_add - Adds two numbers
- * @n1: First number as a string
- * @n2: Second number as a string
+ * @n1: First number
+ * @n2: Second number
  * @r: Buffer to store the result
  * @size_r: Size of the buffer
  *
- * Return: Pointer to the result string, or 0 if the result can't be stored
+ * Return: Pointer to the result (r) or 0 if result cannot be stored
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, sum = 0;
+	int len1 = 0, len2 = 0, max_len, sum, carry = 0;
 	int i, j;
 
-	/* Calculate the lengths of the input strings */
 	while (n1[len1] != '\0')
 		len1++;
 	while (n2[len2] != '\0')
 		len2++;
 
-	/* Check if the result can be stored in r */
-	if (size_r <= len1 || size_r <= len2)
-		return 0;
+	max_len = len1 > len2 ? len1 : len2;
 
-	/* Add the digits from right to left */
-	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--)
+	if (size_r <= max_len + 1)
+		return (0);
+
+	r[max_len] = '\0';
+	len1--;
+	len2--;
+
+	for (i = max_len - 1; i >= 0; i--)
 	{
 		sum = carry;
-		if (i >= 0)
-			sum += n1[i] - '0';
-		if (j >= 0)
-			sum += n2[j] - '0';
+
+		if (len1 >= 0)
+			sum += n1[len1] - '0';
+		if (len2 >= 0)
+			sum += n2[len2] - '0';
 
 		carry = sum / 10;
-		sum %= 10;
+		r[i] = '0' + (sum % 10);
 
-		r[size_r - 1] = sum + '0';
-		size_r--;
+		len1--;
+		len2--;
 	}
 
-	/* If there is still a carry, shift the result to the right */
-	if (carry)
+	if (carry && size_r > max_len)
 	{
-		for (i = size_r - 1; i >= 0; i--)
-			r[i + 1] = r[i];
-		r[0] = carry + '0';
+		for (j = max_len; j >= 0; j--)
+			r[j + 1] = r[j];
+		r[0] = '0' + carry;
+		return (r);
 	}
 
-	return &r[size_r];
+	if (size_r >= max_len)
+		return (r);
+
+	return (0);
 }
