1baf61daf04621eb566b397fb5416fc46d98a005
Hamza <anouihamza@gmail.com>
2023-06-29 12:46:22 -0700

    Pv

---
+++ 0x06-pointers_arrays_strings/rot13.c
@@ -0,0 +1,32 @@
+#include "rot13.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 cipher.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+	int i;
+
+	char rot13[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char ROT13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*ptr)
+	{
+		for (i = 0; rot13[i]; i++)
+		{
+			if (*ptr == rot13[i])
+			{
+				*ptr = ROT13[i];
+				break;
+			}
+		}
+
+		ptr++;
+	}
+
+	return str;
+}
--- 0x06-pointers_arrays_strings/rot13.h
+++ 0x06-pointers_arrays_strings/rot13.h
@@ -1,6 +1,6 @@
 #ifndef ROT13_H
 #define ROT13_H
 
-char *rot13(char *str);
+char *rot13(char *);
 
 #endif /* ROT13_H */
