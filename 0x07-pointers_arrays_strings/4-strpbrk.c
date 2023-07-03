de1070e25bc16af22a5a056d4f4bf136faeed548
Hamza <anouihamza@gmail.com>
2023-07-03 13:52:48 -0700

    Lkd

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,15 +11,15 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-                return (s + i);
-        }
-    }
-    return (NULL);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (NULL);
 }
