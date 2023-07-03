116b10b5e92eae913d40ab41282706b9f17d799c
Hamza <anouihamza@gmail.com>
2023-07-03 13:25:14 -0700

    First

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * simple_print_buffer - Prints buffer in hexadecimal format.
+ * @buffer: The address of memory to print.
+ * @size: The size of the memory to print.
  *
  * Return: Nothing.
  */
@@ -16,13 +16,9 @@ void simple_print_buffer(char *buffer, unsigned int size)
 	while (i < size)
 	{
 		if (i % 10)
-		{
 			printf(" ");
-		}
 		if (!(i % 10) && i)
-		{
 			printf("\n");
-		}
 		printf("0x%02x", buffer[i]);
 		i++;
 	}
@@ -30,7 +26,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 }
 
 /**
- * main - check the code
+ * main - Entry point
  *
  * Return: Always 0.
  */
