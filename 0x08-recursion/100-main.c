2b5b7c9e92dafec55becddafc204f879b130945e
Hamza <anouihamza@gmail.com>
2023-07-06 13:24:53 -0700

    Lke

--- 0x08-recursion/100-main.c
+++ 0x08-recursion/100-main.c
@@ -1,4 +1,4 @@
-#include "100-is_palindrome.c"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -10,16 +10,16 @@ int main(void)
 {
     int r;
 
-    r = is_palindrome("level");
+    r = is_palindrome("abcddcba");
     printf("%d\n", r);
 
-    r = is_palindrome("redder");
+    r = is_palindrome("abcdcba");
     printf("%d\n", r);
 
-    r = is_palindrome("test");
+    r = is_palindrome("abcdecba");
     printf("%d\n", r);
 
-    r = is_palindrome("step on no pets");
+    r = is_palindrome("I guess I thought the dream-space would be all about the visual, but it's more about the feeling. My question is what happens when you start messing with the physics of it.ti fo scisyhp eht htiw gnissem trats uoy nehw sneppah tahw si noitseuq yM .gnileef eht tuoba erom s'ti tub ,lausiv eht tuoba lla eb dluow ecaps-maerd eht thguoht I sseug I");
     printf("%d\n", r);
 
     return (0);
