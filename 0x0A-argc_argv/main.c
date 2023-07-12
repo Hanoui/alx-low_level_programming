5f4f7fc81af501c9e06b6616c39f623c4e273f95
Hamza <anouihamza@gmail.com>
2023-07-12 12:18:18 -0700

    Ghe

---
+++ 0x0A-argc_argv/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: Always 0
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
