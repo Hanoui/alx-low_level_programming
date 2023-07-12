4f9097408ef3a78bfc799a8df8454eee5f522b55
Hamza <anouihamza@gmail.com>
2023-07-12 12:54:58 -0700

    bino

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * is_positive_number - Check if a string represents a positive number
+ * @str: The string to check
+ *
+ * Return: true if the string is a positive number, false otherwise
+ */
+bool is_positive_number(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return false;
+	}
+
+	return true;
+}
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: 0 if successful, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i, num;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (is_positive_number(argv[i]))
+		{
+			num = atoi(argv[i]);
+			sum += num;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
