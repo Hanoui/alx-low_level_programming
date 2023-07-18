592ea950d4324a799821f05c2ecb7acd7fa98379
Hamza <anouihamza@gmail.com>
2023-07-18 16:00:46 -0700

    First

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,9 +14,4 @@ struct dog
     char *owner;
 };
 
-/* Function prototypes */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 #endif /* DOG_H */
-
