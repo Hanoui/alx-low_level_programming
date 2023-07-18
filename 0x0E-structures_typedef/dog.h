11ea45a8acc8e26fe8d5d5a42c6c2561df88e268
Hamza <anouihamza@gmail.com>
2023-07-18 16:22:01 -0700

    free

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,6 +23,13 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/* Define dog_t as a new name for struct dog */
+typedef struct dog dog_t;
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 
 
 
