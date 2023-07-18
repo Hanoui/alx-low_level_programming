9e20caf9361d796d2611da23d8109d874fdfd867
Hamza <anouihamza@gmail.com>
2023-07-18 12:51:13 -0700

    1st

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h> /* Include the string.h header for string operations */
 #include "dog.h"
 
 /**
@@ -10,10 +12,30 @@ int main(void)
 {
     struct dog my_dog;
 
-    my_dog.name = "Poppy";
+    /* Use character arrays to store strings */
+    char name[] = "Poppy";
+    char owner[] = "Bob";
+
+    /* Allocate memory for the name and owner members and copy the strings */
+    my_dog.name = malloc(strlen(name) + 1);
+    my_dog.owner = malloc(strlen(owner) + 1);
+
+    if (my_dog.name == NULL || my_dog.owner == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return (1); /* Return an error code */
+    }
+
+    strcpy(my_dog.name, name);
+    strcpy(my_dog.owner, owner);
+
     my_dog.age = 3.5;
-    my_dog.owner = "Bob";
+
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+
+    /* Don't forget to free the allocated memory */
+    free(my_dog.name);
+    free(my_dog.owner);
+
     return (0);
 }
-
