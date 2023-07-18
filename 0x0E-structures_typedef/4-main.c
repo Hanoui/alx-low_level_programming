4de1364d2ccce2b898ab242c68a4184572665a7d
Hamza <anouihamza@gmail.com>
2023-07-18 16:33:06 -0700

    fere

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -9,21 +9,22 @@
  */
 int main(void)
 {
-    dog_t *my_dog;
+	dog_t *my_dog;
 
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    if (my_dog == NULL)
-    {
-        printf("Failed to create a new dog!\n");
-        return (1);
-    }
+	my_dog = new_dog("Poppy", 3.5, "Bob");
+	if (my_dog == NULL)
+	{
+		printf("Failed to create a new dog!\n");
+		return (1);
+	}
 
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
 
-    /* Remember to free the allocated memory */
-    free(my_dog->name);
-    free(my_dog->owner);
-    free(my_dog);
+	/* Remember to free the allocated memory */
+	free(my_dog->name);
+	free(my_dog->owner);
+	free(my_dog);
 
-    return (0);
+	return (0);
 }
+
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,30 +13,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
+	dog_t *new_dog;
 
-    if (name == NULL || owner == NULL)
-        return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
 
-    /* Allocate memory and copy the name and owner */
-    new_dog->name = malloc(strlen(name) + 1);
-    new_dog->owner = malloc(strlen(owner) + 1);
+	/* Allocate memory and copy the name and owner */
+	new_dog->name = malloc(strlen(name) + 1);
+	new_dog->owner = malloc(strlen(owner) + 1);
 
-    if (new_dog->name == NULL || new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog->owner);
-        free(new_dog);
-        return (NULL);
-    }
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
 
-    strcpy(new_dog->name, name);
-    strcpy(new_dog->owner, owner);
-    new_dog->age = age;
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
+	new_dog->age = age;
 
-    return (new_dog);
+	return (new_dog);
 }
+
