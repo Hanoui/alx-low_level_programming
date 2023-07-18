b84da98469610c7e77a08ff78349d5bf1e290c3f
Hamza <anouihamza@gmail.com>
2023-07-18 16:27:47 -0700

    gher

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -30,6 +30,15 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/* Define dog_t as a new name for struct dog */
+typedef struct dog dog_t;
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+
 
 
 
