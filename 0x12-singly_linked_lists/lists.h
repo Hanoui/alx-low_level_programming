d8649a0b2344258fd35fafd67b1ec639e75f7265
Hanoui <slayeroma@hotmail.com>
2023-07-30 14:52:32 -0700

    hgf

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,6 +25,12 @@ size_t print_list(const list_t *h);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 
+/* Function prototypes */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+
+
 
 
 #endif /* LISTS_H */
