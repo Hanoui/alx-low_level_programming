28b974f3c7214b25534d3ed34bc9ff1f1b366da7
Hanoui <slayeroma@hotmail.com>
2023-08-02 14:52:31 -0700

    lkj

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,33 +2,44 @@
 #include <stdio.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely (handles loops).
- * @head: Pointer to the head of the list.
+ * print_listint_safe - Prints a listint_t linked list safely
+ * @head: Pointer to the head of the list
  *
- * Return: The number of nodes in the list.
+ * Return: Number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;
-    const listint_t *current = head;
-    const listint_t *loop_node = NULL;
+	size_t num_nodes = 0;
+	const listint_t *current = head;
+	const listint_t *temp;
 
-    while (current)
-    {
-        printf("[%p] %d\n", (void *)current, current->n);
-        count++;
+	while (current != NULL)
+	{
+		num_nodes++;
+		putchar('[');
+		printf("%p", (void *)current);
+		putchar(']');
+		printf(" %d\n", current->n);
+		current = current->next;
+		temp = head;
+		while (num_nodes > 0)
+		{
+			if (temp == current)
+			{
+				putchar('-');
+				putchar('>');
+				putchar(' ');
+				putchar('[');
+				printf("%p", (void *)current);
+				putchar(']');
+				printf(" %d\n", current->n);
+				return (num_nodes);
+			}
+			num_nodes--;
+			temp = temp->next;
+		}
+	}
 
-        if (current == loop_node)
-        {
-            printf("-> [%p] %d\n", (void *)loop_node, loop_node->n);
-            break;
-        }
-
-        current = current->next;
-        if (count % 2 == 0 && count != 0)
-            loop_node = loop_node ? loop_node->next : head;
-    }
-
-    return (count);
+	return (num_nodes);
 }
 
