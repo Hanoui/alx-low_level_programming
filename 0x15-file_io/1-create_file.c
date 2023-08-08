7b955e3a6b02b17a6183c9715a51c9cc3c3e61a2
Hanoui <slayeroma@hotmail.com>
2023-08-08 12:16:14 -0700

    lkj

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC,
+	S_IRUSR | S_IWUSR);
 	if (file_descriptor == -1)
 		return (-1);
 
@@ -21,6 +22,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[length] != '\0')
 			length++;
+
 		write_count = write(file_descriptor, text_content, length);
 		if (write_count != length)
 		{
